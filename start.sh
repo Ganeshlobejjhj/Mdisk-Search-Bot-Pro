f908889e36d42434a1ae584878f02c64c35c3f30
Ganeshlobejjhj <118112179+Ganeshlobejjhj@users.noreply.github.com>
2023-03-05 20:08:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/Link-Search-Bot-Pro /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/Ganeshlobejjhj/mdiak-search-bot-pro/mdisk-search-bot-pro
+cd /Mdisk-search-bot-pro 
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
